bb888377e951137afeb5d548ebf36a191b0f7cf0
ann <ann30236@gmail.com>
2023-10-24 15:54:55 +0800

    modify3

--- project1/lesson3.py
+++ project1/lesson3.py
@@ -1,1 +1,16 @@
-print("Hello python")
+
+import tkinter as tk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Hello Tkinter")
+
+        label = tk.Label(self, text="Hello World!")
+        label.pack(fill=tk.BOTH, expand=1, padx=100, pady=50)
+
+
+if __name__ == "__main__":
+    window = Window()
+    window.mainloop()
+        
