35cd878de45713ba90ff042da9f9ffe1ad3d1211
ann <ann30236@gmail.com>
2023-10-24 16:01:40 +0800

    modify4

--- project1/lesson3.py
+++ project1/lesson3.py
@@ -1,4 +1,3 @@
-
 import tkinter as tk
 
 class Window(tk.Tk):
@@ -6,11 +5,23 @@ class Window(tk.Tk):
         super().__init__()
         self.title("Hello Tkinter")
 
-        label = tk.Label(self, text="Hello World!")
-        label.pack(fill=tk.BOTH, expand=1, padx=100, pady=50)
+        self.label = tk.Label(self, text="Choose One")
+        self.label.pack(fill=tk.BOTH, expand=1, padx=100, pady=30)
+
+        hello_button = tk.Button(self, text="Say Hello", command=self.say_hello)
+        hello_button.pack(side=tk.LEFT, padx=(20, 0), pady=(0, 20))
+
+        goodbye_button = tk.Button(self, text="Say Goodbye", command=self.say_goodbye)
+        goodbye_button.pack(side=tk.RIGHT, padx=(0, 20), pady=(0, 20))
+
+    def say_hello(self):
+        self.label.configure(text="Hello World!")
+
+    def say_goodbye(self):
+        self.label.configure(text="Goodbye! \n (Closing in 2 seconds)")
+        self.after(2000, self.destroy)
 
 
 if __name__ == "__main__":
     window = Window()
     window.mainloop()
-        
